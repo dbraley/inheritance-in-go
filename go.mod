87a3844cb242b4eed33eea1e3da7f54dd93edb07
David Braley <david.braley@gmail.com>
2019-08-19 17:31:05 -0400

    Initialize basic setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dbraley/inheritance-in-go
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Here we should initialize an in memory data store...")
+	fmt.Println("Here we should query that data store for admin accounts, then for each print the email, and a list of login user names, and ip addresses")
+}
